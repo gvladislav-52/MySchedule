ac1447cc7348cc725e8887e9fe3b8008227d051d
gvladislav-52 <gvladislav_52@mail.ru>
2024-07-07 16:10:29 +0300

    Added button, calendar swipes

--- ScheduleViewController.swift
+++ ScheduleViewController.swift
@@ -10,10 +10,21 @@ import FSCalendar
 
 class ScheduleViewController: UIViewController {
     
+    var calendarHeightConstraint: NSLayoutConstraint!
+    
     private var calendar: FSCalendar = {
             let calendar = FSCalendar()
             calendar.translatesAutoresizingMaskIntoConstraints = false
             return calendar
+    }()
+    
+    let showHideButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Open calender", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
 
     override func viewDidLoad() {
@@ -21,19 +32,89 @@ class ScheduleViewController: UIViewController {
         view.backgroundColor = .white
         title = "Schedule"
         
-        setConstraints()
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+        setupCalendar()
+        setupButton()
+        swipeAction()
     }
 }
 
-//MARK: - SetContrraints
+//MARK: - FSCalendarDataSource, FSCalendarDelegate
+extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
+
 extension ScheduleViewController {
-    func setConstraints() {
+    
+    //MARK: - setupLayout
+    func setupCalendar() {
         view.addSubview(calendar)
+        calendar.scope = .week
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        
+        calendar.addConstraint(calendarHeightConstraint)
+        
         NSLayoutConstraint.activate([
             calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
             calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            calendar.heightAnchor.constraint(equalToConstant: 300)
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
         ])
     }
+    
+    func setupButton() {
+        view.addSubview(showHideButton)
+        
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            showHideButton.widthAnchor.constraint(equalToConstant: 100),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func showHideButtonTapped() {
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Close calender", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Open calender", for: .normal)
+        }
+    }
+    
+    
+    //MARK: - SwipeGestureRecognizer
+    func swipeAction() {
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        calendar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case .up:
+            showHideButtonTapped()
+        case .down:
+            showHideButtonTapped()
+        default:
+            break
+        }
+    }
 }
+c
